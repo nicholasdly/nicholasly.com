ca3f83db7897a4e26f342ea983d8f085686c6862
Nicholas Ly <nichdly@gmail.com>
2024-04-14 18:18:19 -0500

    wip

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,12 +15,12 @@ export function formatDateString(str: string) {
 }
 
 async function getPosts(): Promise<Post[]> {
-  const dirents = await readdir("./src/app/blog", { withFileTypes: true });
+  const dirents = await readdir("./src/app/blog/(posts)/", { withFileTypes: true });
   const slugs = dirents.filter((dirent) => dirent.isDirectory());
 
   const posts = await Promise.all(
     slugs.map(async ({ name }) => {
-      const { metadata } = await import(`../blog/${name}/page.mdx`);
+      const { metadata } = await import(`../blog/(posts)/${name}/page.mdx`);
       return { slug: name, ...metadata };
     })
   );
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,15 +1,15 @@
 type TitleProps = {
   title: string;
-  date?: string;
+  publishDate?: string;
 };
 
-export default function Title({ title, date }: TitleProps) {
+export default function Title({ title, publishDate }: TitleProps) {
   return (
     <div className="not-prose mb-8">
-      <h1 className="text-black dark:text-white font-medium text-2xl">
+      <h1 className="text-black dark:text-white font-medium text-2xl mb-1">
         {title}
       </h1>
-      {date && <span className="text-sm">{date}</span>}
+      {publishDate && <p className="text-sm">{publishDate}</p>}
     </div>
   );
 }
