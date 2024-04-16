44a89af9f2aef6a990fced599298db2635756038
Nicholas Ly <nichdly@gmail.com>
2024-04-15 19:21:11 -0500

    feat: prettier, integrations, fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://nicholasly.com/",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    icon(),
+    robotsTxt({
+      sitemap: [
+        "https://nicholasly.com/sitemap-index.xml",
+        "https://nicholasly.com/sitemap-0.xml",
+      ],
+    }),
+  ],
 });
